ae628231404f63013e4f3ae1a8f24d7ae89f8cca
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-21 18:49:00 -0500

    got framework for the app done

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const i = document.querySelector('#in');
 const btn = document.querySelector('#btn');
 const sec = document.querySelector('#sec');
+const ass = document.querySelector('#ass');
 
-const names = {};
+var names = {};
 
 const display = () => {
-  for (const name of names) {
-    sec.innerHTML += `<p>${name}`;
+  // iterate through keys and make paragraph tag showing name and money owed
+  sec.innerHTML = '';
+  for (const name of Object.keys(names)) {
+    sec.innerHTML += `<p>${name}: ${Math.round(names[name] * 100) / 100}</p>`;
   }
 };
 
 const addPerson = () => {
-  const in_ = i.split(',');
+  const in_ = i.value.split(',');
   const name = in_[0];
+  let money;
   if (in_.length == 2) {
-    const money = parseInt(in_[1]);
+    money = parseFloat(in_[1]);
+  } else {
+    money = 0;
   }
+  money *= ass.value == 'sub' ? -1 : 1;
 
   if (!(name in names)) {
-    names.append(name);
+    if (!(money == null)) {
+      names = { ...names, [name]: money };
+    }
+  } else {
+    names[name] += money;
   }
+
+  display();
 };
-uhj89ihk;
+
+btn.onclick = addPerson;
