7554a403fe1677abb3582923b56840c86f63f991
neelyp <160043302+neelyp@users.noreply.github.com>
2024-02-21 08:02:20 -0500

    get input

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const i = document.querySelector("#in");
-const btn = document.querySelector("#btn");
+const i = document.querySelector('#in');
+const btn = document.querySelector('#btn');
+const sec = document.querySelector('#sec');
+
+const names = {};
+
+const display = () => {
+  for (const name of names) {
+    sec.innerHTML += `<p>${name}`;
+  }
+};
 
 const addPerson = () => {
- alert(typeof i.value);
-}
+  const in_ = i.split(',');
+  const name = in_[0];
+  if (in_.length == 2) {
+    const money = parseInt(in_[1]);
+  }
 
-
+  if (!(name in names)) {
+    names.append(name);
+  }
+};
+uhj89ihk;
